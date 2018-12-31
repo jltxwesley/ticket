2bd9a20b1b7cfe0fccc27097f4ab151708798b33
Xu Zhang <jltxwesley@gmail.com>
2018-12-31 16:37:26 +1100

    refusing orders when there are no more tickets

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Concert;
+use App\Exceptions\NotEnoughTicketsException;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -61,6 +62,7 @@ class ConcertTest extends TestCase
     public function can_order_tickets()
     {
         $concert = factory(Concert::class)->create();
+        $concert->addTickets(3);
 
         $order = $concert->orderTickets('jane@example.com', 3);
 
@@ -87,5 +89,45 @@ class ConcertTest extends TestCase
         $order = $concert->orderTickets('jane@example.com', 30);
 
         $this->assertEquals(20, $concert->ticketsRemaining());
+    }
+
+    /** @test */
+    public function trying_to_purchase_more_tickets_than_remain_thows_an_exception()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(10);
+
+        try {
+            $order = $concert->orderTickets('jane@example.com', 11);
+        } catch (NotEnoughTicketsException $e) {
+            $order = $concert->orders()->where('email', 'jane@example.com')->first();
+            $this->assertNull($order);
+            $this->assertEquals(10, $concert->ticketsRemaining());
+
+            return;
+        }
+
+        $this->fail('Order succeeded even though there were not enough tickets remaining.');
+    }
+
+    /** @test */
+    public function cannot_order_tickets_that_have_already_been_purchased()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(10);
+
+        $order = $concert->orderTickets('jane@example.com', 8);
+
+        try {
+            $order = $concert->orderTickets('john@example.com', 3);
+        } catch (NotEnoughTicketsException $e) {
+            $order = $concert->orders()->where('email', 'john@example.com')->first();
+            $this->assertNull($order);
+            $this->assertEquals(2, $concert->ticketsRemaining());
+
+            return;
+        }
+
+        $this->fail('Order succeeded even though there were not enough tickets remaining.');
     }
 }
