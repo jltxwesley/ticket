e825b5af2fe81bb42e9114cbc0775f995710bffe
Xu Zhang <jltxwesley@gmail.com>
2019-01-02 14:55:48 +1100

    persisting the order amount - real refactoring, keep green all the time?

--- database/migrations/2018_12_19_124551_create_orders_table.php
+++ database/migrations/2018_12_19_124551_create_orders_table.php
@@ -16,6 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('concert_id');
+            $table->unsignedInteger('amount');
             $table->string('email');
             $table->timestamps();
         });
