67d7c1d6649af82072495af0f7871b2cd6447719
Xu Zhang <jltxwesley@gmail.com>
2019-01-12 14:44:52 +1100

    a segfault

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -115,13 +115,29 @@ class PurchaseTicketsTest extends TestCase
     /** @test */
     public function cannot_purchase_tickets_another_customer_is_already_trying_to_purchase()
     {
-        $concert = factory(Concert::class)->state('published')->create()->addTickets(3);
+        $concert = factory(Concert::class)->state('published')->create(['ticket_price' => 1200])->addTickets(3);
+
+        $this->paymentGateway->beforeFirstCharge(function ($paymentGateway) use ($concert) {
+            $response = $this->orderTickets($concert, [
+                'email'           => 'personB@exmaple.com',
+                'ticket_quantity' => 1,
+                'payment_token'   => $this->paymentGateway->getValidTestToken()
+            ]);
+
+            $response->assertStatus(422);
+            $this->assertFalse($concert->hasOrderFor('personB@exmaple.com'));
+            $this->assertEquals(0, $this->paymentGateway->totalCharges());
+        });
 
         $response = $this->orderTickets($concert, [
             'email'           => 'personA@exmaple.com',
-            'ticket_quantity' => 1,
+            'ticket_quantity' => 3,
             'payment_token'   => $this->paymentGateway->getValidTestToken()
         ]);
+
+        $this->assertEquals(3600, $this->paymentGateway->totalCharges());
+        $this->assertTrue($concert->hasOrderFor('personA@exmaple.com'));
+        $this->assertEquals(3, $concert->ordersFor('personA@exmaple.com')->first()->ticketsQuantity());
     }
 
     /** @test */
